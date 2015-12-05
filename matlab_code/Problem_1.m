65cabdfb316e2a83492a2842545ba0371651f82c
Ryan Skinner <anomaly1452@gmail.com>
2015-12-05 00:48:57 -0700

    Touch-up to Problem 2 code.

--- matlab_code/Problem_1.m
+++ matlab_code/Problem_1.m
@@ -9,16 +9,22 @@ function [] = Problem_1()
     
     Set_Default_Plot_Properties();
     
+    for IC_str = {'a','b'}
+    for n = [5,6]
+    
     % Spatial domain.
-    n = 5;
     nn = 2^n + 1;
     Ly = 2.0;
     y = linspace(0, Ly, nn)';
     
     % Temporal domain.
     dt = 0.001;
-    tfinal = 1;
-    t = [0:dt:tfinal];
+    if strcmp(IC_str,'a')
+        t_final = 2;
+    else
+        t_final = 2;
+    end
+    t = [0:dt:t_final];
     
     % Physical parameters.
     Re = 1;
@@ -28,22 +34,25 @@ function [] = Problem_1()
 
     % Initialize solution.
     T = nan(nn,length(t));
-%     T(:,1) = cos(2*pi*y) .* sin(pi*y); IC_str = '(a)';
-    T(:,1) = cos(2*pi*y); IC_str = '(b)';
+    if strcmp(IC_str,'a')
+        T(:,1) = cos(2*pi*y) .* sin(pi*y);
+    else
+        T(:,1) = cos(2*pi*y);
+    end
 
     %%%
     % Solve problem numerically.
     %%%
     
-    for time_n = 1:(length(t)-1)
+    for t_n = 1:(length(t)-1)
         
-        T_n = T(:,time_n);
+        T_n = T(:,t_n);
         
         dTdy   = find_dfdn(  T_n',nn,Ly)';
         d2Tdy2 = find_d2fdn2(T_n',nn,Ly)';
         
         % Update solution.
-        T(:,time_n+1) = T_n + dt * (alpha * d2Tdy2 - v .* dTdy);
+        T(:,t_n+1) = T_n + dt * (alpha * d2Tdy2 - v .* dTdy);
 
     end
 
@@ -51,26 +60,22 @@ function [] = Problem_1()
     % Process results.
     %%%
     
-%     surf(y,t,T');
-%     xlabel('y');
-%     ylabel('t');
-    
     % Time evolution of solution for a single Courant number.
-    n_plot = 21;
+    n_plot = 31;
     cmap = winter(n_plot);
     step_numbers = round(linspace(1,length(t),n_plot));
     hf = figure();
     set(hf,'Position',[100,500,900,300]);
     hold on;
     plot_handles = [];
-    for time_n = 1:length(step_numbers)
-        tmp = sprintf('t = %.2f', t(step_numbers(time_n)));
-        hp = plot(y, T(:,step_numbers(time_n)), 'DisplayName', tmp, 'Color', cmap(time_n,:));
-        if(mod(time_n-1,4) == 0)
+    for t_n = 1:length(step_numbers)
+        tmp = sprintf('t = %.2f', t(step_numbers(t_n)));
+        hp = plot(y, T(:,step_numbers(t_n)), 'DisplayName', tmp, 'Color', cmap(t_n,:));
+        if(mod(t_n-1,5) == 0)
             plot_handles(end+1) = hp;
         end
     end
-    title(sprintf('IC = %s, n = %.0f',IC_str,n));
+    title(sprintf('IC = (%s), n = %.0f',IC_str{1},n));
     xlabel('x');
     ylabel('u');
     ylim([-1,1]);
@@ -78,8 +83,10 @@ function [] = Problem_1()
     hleg = legend(plot_handles);
     set(hleg,'Location','eastoutside');
     
+    end
+    end
+    
     disp('Done.');
-    return
     
 end
 
